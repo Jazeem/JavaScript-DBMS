dc2209d17be111b21103110e984c993041e00e33
Nithin <nithinknitin@gmail.com>
2015-04-21 05:45:32 +0930

    insert done

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var JDBMS = angular.module('JDBMS',[]);
 JDBMS.controller('MainCtrl',function($scope, $http){
 	$http.get('database.json').success(function(data) {
     $scope.data = data;
+    $scope.tupleToInsert;
   	});
 	$scope.displayTable = function(table){
 		$scope.selectedTable = table;
+	}
+	$scope.insertTuple = function(){
+		var obj = JSON.parse($scope.tupleToInsert);
+		$scope.selectedTable.data.push(obj);
 	}
 });
