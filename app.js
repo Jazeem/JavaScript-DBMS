a3a7f20445d357a7d81860817593538a66ad47b8
Jazeem <jazeem23@gmail.com>
2015-04-20 23:59:04 +0530

    basic

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var JDBMS = angular.module('JDBMS',[]);
 
 
-JDBMS.controller('MainCtrl',function($scope,$http){
-
-	
+JDBMS.controller('MainCtrl',function($scope, $http){
+	$http.get('database.json').success(function(data) {
+    $scope.data = data;
+  	});
+	$scope.displayTable = function(table){
+		$scope.selectedTable = table;
+	}
 });
