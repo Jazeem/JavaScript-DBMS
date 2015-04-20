07173c90597ed80424fd03beef8b06091097b759
Jazeem <jazeem23@gmail.com>
2015-04-21 03:24:14 +0530

    add table, drop table

--- app.js
+++ app.js
@@ -37,5 +37,37 @@ JDBMS.controller('MainCtrl',function($scope, $http){
 	$scope.insertTuple = function(){
 		var obj = JSON.parse($scope.tupleToInsert);
 		$scope.selectedTable.data.push(obj);
+		console.log($scope.selectedTable);
+	}
+
+	$scope.addTable = function(){
+		var queryFinal = '{'+$scope.tableQuery+',"data": []}';
+		//console.log(queryFinal);
+		var obj = JSON.parse(queryFinal);
+		$scope.data.push(obj);
+		$scope.tableQuery='';
+	}
+
+	$scope.dropTable = function(){
+		var tableName = $scope.selectedTable.name;
+		for (var i = $scope.data.length - 1; i >= 0; i--) {
+			if($scope.data[i] == $scope.selectedTable)
+				break;
+		}
+		$scope.data.splice(i, 1);
+		var i, j;
+		for (i = $scope.data.length - 1; i >= 0; i--) {
+			for (j = $scope.data[i].schema.length - 1; j >= 0; j--) {
+				if($scope.data[i].schema[j].hasOwnProperty('foreign_key') && $scope.data[i].schema[j].foreign_key === tableName)
+					break;
+			}
+			if(j >= 0){
+				$scope.data[i].schema.splice(j, 1);
+				for (var k = 0; k < $scope.data[i].data.length; k++) {
+					$scope.data[i].data[k].splice(j, 1);	
+				}
+			}			
+		}
+		$scope.selectedTable = '';
 	}
 });
